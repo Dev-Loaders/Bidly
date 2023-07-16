d4cc8b926cfc773359b946658948f189b079ce05
dIB59 <ibrahim@ebs.net.pk>
2023-07-16 12:14:15 +0200

    added crossOrigin to the controller

--- src/main/java/com/bidly/bidly/user/Controller.java
+++ src/main/java/com/bidly/bidly/user/Controller.java
@@ -2,13 +2,11 @@ package com.bidly.bidly.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/")
+@CrossOrigin(origins = "bodly.vercel.app")
 public class Controller {
 
     private final BidlyService service;
