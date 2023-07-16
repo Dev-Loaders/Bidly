b05578733e8278976dbdaf548075ed07dd915379
Vanessa Wingårdh <vwingardh@gmail.com>
2023-07-16 09:40:09 +0200

    Created User entity class

---
+++ src/main/java/com/bidly/bidly/user/User.java
@@ -0,0 +1,60 @@
+package com.bidly.bidly.user;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+    @Column(name = "jwt_id")
+    private String jwtId;
+    @Column(name = "full_name")
+    private String fullName;
+    @Column(name = "email")
+    private String email;
+
+    public User() {
+
+    }
+    public User(Long id, String jwtId, String fullName, String email) {
+        this.id = id;
+        this.jwtId = jwtId;
+        this.fullName = fullName;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getJwtId() {
+        return jwtId;
+    }
+
+    public void setJwtId(String jwtId) {
+        this.jwtId = jwtId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
