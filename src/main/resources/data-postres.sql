c6f9ac7e8eec091b089472cc8c2e7fffae69befe
dIB59 <ibrahim@ebs.net.pk>
2023-07-16 16:02:36 +0200

    setup test enviroment

--- src/main/resources/data-postres.sql
+++
@@ -1,1 +0,0 @@
-INSERT into bidly_user (jwt_id, full_name, email) values ('1', 'Test Tester', 'test@test.com') ON CONFLICT DO NOTHING;
---
+++ src/test/resources/data-postres.sql
@@ -0,0 +1,1 @@
+-- INSERT into bidly_user (jwt_id, full_name, email) values ('1', 'Test Tester', 'test@test.com') ON CONFLICT DO NOTHING;
