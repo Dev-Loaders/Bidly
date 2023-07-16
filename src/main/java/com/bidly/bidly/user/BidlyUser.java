75e8a240a7bb39ecf132eac5a3fec9f72d79f2a5
Vanessa Wingårdh <vwingardh@gmail.com>
2023-07-16 10:14:00 +0200

    Fixed: Changed BidlyUser entity class name

--- src/main/java/com/bidly/bidly/user/BidlyUser.java
+++ src/main/java/com/bidly/bidly/user/BidlyUser.java
@@ -3,23 +3,24 @@ package com.bidly.bidly.user;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "user")
-public class User {
+@Table(name = "bidly_user")
+public class BidlyUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id", nullable = false)
+    @Column(name = "bidly_id")
     private Long id;
-    @Column(name = "jwt_id", nullable = false)
+    @Column(name = "jwt_id")
     private String jwtId;
-    @Column(name = "full_name", nullable = false)
+    @Column(name = "full_name")
     private String fullName;
-    @Column(name = "email", nullable = false)
+    @Column(name = "email")
     private String email;
 
-    public User() {
+    public BidlyUser() {
 
     }
-    public User(Long id, String jwtId, String fullName, String email) {
+
+    public BidlyUser(Long id, String jwtId, String fullName, String email) {
         this.id = id;
         this.jwtId = jwtId;
         this.fullName = fullName;
@@ -28,10 +29,6 @@ public class User {
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getJwtId() {
--- src/main/resources/data-postres.sql
+++ src/main/resources/data-postres.sql
@@ -1,1 +1,1 @@
-INSERT into person (name) values ('Food') ON CONFLICT DO NOTHING;
+INSERT into user (user_id, jwtId, fullName, email) values (1, '1', 'Test Tester', 'test@test.com') ON CONFLICT DO NOTHING;
