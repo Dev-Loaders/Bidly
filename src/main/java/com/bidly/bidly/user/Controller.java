bdf956f2c7e47471fe68f56e4e196c00963deff9
Vanessa Wingårdh <vwingardh@gmail.com>
2023-07-16 10:53:32 +0200

    Updated controller with getUser() for testing

--- src/main/java/com/bidly/bidly/user/Controller.java
+++ src/main/java/com/bidly/bidly/user/Controller.java
@@ -1,15 +1,30 @@
 package com.bidly.bidly.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/")
 public class Controller {
+
+    private final BidlyService service;
+
+    @Autowired
+    public Controller(BidlyService service) {
+        this.service = service;
+    }
 
     @GetMapping("hello")
     public String getHello() {
         return "hello";
+    }
+
+    @GetMapping("user")
+    public BidlyUser getUser() {
+        return service.getUser(1L);
     }
 }
