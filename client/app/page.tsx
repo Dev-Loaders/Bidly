5fcf990cbea869b4c03899e0506378118d7ca9fe
Vanessa Wingårdh <vwingardh@gmail.com>
2023-07-16 11:09:42 +0200

    Client passes hello + user test

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,31 @@
 import Image from 'next/image'
 
-export default function Home() {
+const getHello = async () => {
+  const response = await fetch('http://localhost:8080/api/hello');
+  if (!response.ok) {
+    throw new Error("Could not fetch");
+  }
+  const text = await response.text();
+  return text;
+}
+
+const getUser = async () => {
+  const response = await fetch('http://localhost:8080/api/user');
+  if (!response.ok) {
+    throw new Error("Could not fetch");
+  }
+  const text = await response.text();
+  return text;
+}
+
+export default async function Home() {
+  const str = await getHello();
+  const user = await getUser();
+
   return (
     <main>
-      <div>Hello</div>
+      <div>{str}</div>
+      <div>{user}</div>
     </main>
   )
 }
