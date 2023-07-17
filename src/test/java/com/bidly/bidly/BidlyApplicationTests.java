4a57b505cd87cb091c16b6020178c187448ab479
dIB59 <ibrahim@ebs.net.pk>
2023-07-17 09:15:16 +0200

    setup testing

--- src/test/java/com/bidly/bidly/BidlyApplicationTests.java
+++ src/test/java/com/bidly/bidly/BidlyApplicationTests.java
@@ -2,8 +2,14 @@ package com.bidly.bidly;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class BidlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
 
 }
