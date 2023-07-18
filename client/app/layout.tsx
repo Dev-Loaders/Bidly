91e74b4fd3ee256b96067ba74ef00393883453be
Vanessa Wingårdh <vwingardh@gmail.com>
2023-07-18 15:13:27 +0200

    Created global head element

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,4 +1,5 @@
-import './globals.css'
+// import './globals.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
@@ -16,6 +17,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width" />
+        <title>Bidly</title>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+          integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
