a3ce5811b6c9e1c3000ffde7c8e92254e924a475
Vanessa Wingårdh <vwingardh@gmail.com>
2023-07-18 13:36:21 +0200

    Created route to workspace/user-jobs

---
+++ client/app/workspace/user-jobs/page.tsx
@@ -0,0 +1,8 @@
+export default function UserJobs() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center p-24">
+            <div>this will get all jobs user has created - getJobsByUserId()</div>
+            <div>GET /api/users/user_id/jobs</div>
+        </main>
+    )
+}
