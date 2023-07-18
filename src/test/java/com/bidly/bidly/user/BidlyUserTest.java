2e607c4b20118149a5747b44a54f6fdc2a6b63af
dIB59 <ibrahim@ebs.net.pk>
2023-07-18 10:12:42 +0200

    made the test more rigorous

--- src/test/java/com/bidly/bidly/user/BidlyUserTest.java
+++ src/test/java/com/bidly/bidly/user/BidlyUserTest.java
@@ -1,25 +1,30 @@
-package com.bidly.bidly;
+package com.bidly.bidly.user;
 
-import com.bidly.bidly.user.BidlyService;
-import com.bidly.bidly.user.BidlyUser;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class BidlyApplicationTests {
+class BidlyUserTest {
 
     @Autowired
     BidlyService service;
 
+    BidlyUser user = new BidlyUser(1L,"1", "Test Tester", "test@test.com");
+
     @Test
     void serviceTest() {
-        BidlyUser var = service.getUser(1L);
-        System.out.println(var);
-        assertNotEquals(var, 1L);
+        BidlyUser userDB = service.getUser(1L);
+        System.out.println(userDB.getFullName());
+        Assertions.assertNotNull(userDB);
+        assertEquals(user.getFullName(), userDB.getFullName());
+        assertEquals(user.getJwtId(), userDB.getJwtId());
+        assertEquals(user.getEmail(), userDB.getEmail());
 
     }
 
