ad28250e76a9da651324ea32e62dbff0e06feb8d
Vanessa Wingårdh <vwingardh@gmail.com>
2023-07-18 13:36:35 +0200

    Created route to workspace/user-bids

---
+++ client/app/workspace/user-bids/page.tsx
@@ -0,0 +1,8 @@
+export default function UserBids() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center p-24">
+            <div>this will get all jobs user bid on - getBidsByUserId()</div>
+            <div>GET /api/users/user_id/bids</div>
+        </main>
+    )
+}
