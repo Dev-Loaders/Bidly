da8f4b200e279802a7550d155f35dcb7ec9f08d0
dIB59 <ibrahim@ebs.net.pk>
2023-07-19 11:30:39 +0200

    now adds jobs to user

--- src/main/java/com/bidly/bidly/user/BidlyUserService.java
+++ src/main/java/com/bidly/bidly/user/BidlyUserService.java
@@ -1,7 +1,12 @@
 package com.bidly.bidly.user;
 
+import com.bidly.bidly.job.Job;
+import com.bidly.bidly.job.JobRequest;
+import com.bidly.bidly.job.JobRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,13 +14,26 @@ import org.springframework.stereotype.Service;
 public class BidlyUserService {
 
     private final BidlyUserRepository userRepo;
+    private final JobRepository jobRepo;
 
     @Autowired
-    public BidlyUserService(BidlyUserRepository userRepo) {
+    public BidlyUserService(BidlyUserRepository userRepo, JobRepository jobRepo) {
         this.userRepo = userRepo;
+        this.jobRepo = jobRepo;
     }
 
-    public BidlyUser getUser(Long jwtId) {
-        return userRepo.getUser(jwtId);
+    public BidlyUser getUserByJwtId(String userSubject) {
+        return userRepo.getUserByJwtId(userSubject);
+    }
+
+    public void createUser(OidcUser oidcUser) {
+        userRepo.createUser(oidcUser);
+    }
+
+    public ResponseEntity<Job> addJobPostToUser(String userSubject, JobRequest jobPost) {
+        Job job = jobRepo.createJob(userSubject, jobPost);
+        userRepo.updateUser(job, userSubject);
+        return ResponseEntity.ok(job);
+
     }
 }
