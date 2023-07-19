94ef9fff7af6083521a85fe9f0b8269a67780356
Vanessa Wingårdh <vwingardh@gmail.com>
2023-07-19 14:42:21 +0200

    Created success handler for redirecting upon signup/login

---
+++ src/main/java/com/bidly/bidly/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,22 @@
+package com.bidly.bidly;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        String token = (String) authentication.getPrincipal();
+        HttpSession session = request.getSession();
+        session.setAttribute("token", token);
+        String redirectUrl = (String) session.getAttribute("redirectUrl");
+        response.sendRedirect(redirectUrl);
+    }
+}
