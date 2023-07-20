5ef47c00586155a6f4f43e3afb1ddcc84ff6093b
dIB59 <ibrahim@ebs.net.pk>
2023-07-20 09:52:29 +0200

    added Jwt validation to Success handler

--- src/main/java/com/bidly/bidly/security/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/bidly/bidly/security/CustomAuthenticationSuccessHandler.java
@@ -3,6 +3,7 @@ package com.bidly.bidly.security;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -12,9 +13,18 @@ import java.io.IOException;
 
 @Component
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final JwtValidation validation;
+
+    @Autowired
+    public CustomAuthenticationSuccessHandler(JwtValidation validation){
+        this.validation = validation;
+    }
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         OidcUser token = (OidcUser) authentication.getPrincipal();
+        validation.validateJwt(token);
         HttpSession session = request.getSession();
         session.setAttribute("token", token);
         response.sendRedirect("https://bidly.vercel.app/workspace");
