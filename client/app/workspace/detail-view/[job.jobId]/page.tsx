10acf8a1418f1086ac21dd4be98ad77cb4876691
Vanessa Wingårdh <vwingardh@gmail.com>
2023-07-20 16:09:29 +0200

    Updated DetailView() to show job by id and BidForm

--- client/app/workspace/detail-view/[job.jobId]/page.tsx
+++ client/app/workspace/detail-view/[job.jobId]/page.tsx
@@ -1,31 +1,59 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import BidForm from "./BidForm";
+import Card from 'react-bootstrap/Card';
+
+type Job = {
+  title: string;
+  location: string;
+  image: string;
+  materials: boolean;
+  description: string;
+  jobId: string;
+};
 
 export default function DetailView() {
-    const jobId = window.location.href.split("/")[5];
-    const token = sessionStorage.getItem("token");
+  const jobId = window.location.href.split("/")[5];
+  const token = sessionStorage.getItem("token");
 
-    const [jobDetails, setJobDetails] = useState<Job | null>(null);
+  const [jobDetails, setJobDetails] = useState<Job | null>(null);
 
-    useEffect(() => {
-        axios.get(`http://localhost:8080/api/jobs/${jobId}`, {
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/api/jobs/${jobId}`, {
         headers: {
-            "Content-Type": "application/json",
-            Authorization: "Bearer ",
+          Authorization: "Bearer ",
         },
-        })
-        .then((response) => {
-        setJobDetails(response.data)
-        })
-        .catch((error) => {
+      })
+      .then((response) => {
+        setJobDetails(response.data);
+      })
+      .catch((error) => {
         console.error(error);
-        });
-    }, [jobId]);
+      });
+  }, [jobId]);
 
   return (
-    <div>
-      <h1>{jobDetails?.title}</h1>
-    </div>
+    <>
+      <Card className="mt-5">
+        <Card.Body>
+          <Card.Title>{jobDetails?.title}</Card.Title>
+          <Card.Text>
+            <strong>Location:</strong> {jobDetails?.location}
+          </Card.Text>
+          <Card.Img variant="top" src={jobDetails?.image} />
+          <Card.Text>
+            <strong>Materials:</strong>{" "}
+            {jobDetails?.materials ? "Provided" : "Not Provided"}
+          </Card.Text>
+          <Card.Text>
+            <strong>Description:</strong> {jobDetails?.description}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+
+      {jobDetails && <BidForm jobId={jobDetails.jobId} />}
+    </>
   );
 }
