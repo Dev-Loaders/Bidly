c1f73db34b1f28a1947602c47a7c306d5a571b04
Vanessa Wingårdh <vwingardh@gmail.com>
2023-07-21 12:02:20 +0200

    Removed default text that was on page

--- client/app/workspace/post-job/page.tsx
+++ client/app/workspace/post-job/page.tsx
@@ -3,8 +3,6 @@ import { JobForm } from "./JobForm";
 export default function PostJob() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-24">
-            <div>this will be a form to post job - postJob()</div>
-            <div>POST /api/jobs</div>
             <JobForm></JobForm>
         </main>
     )
