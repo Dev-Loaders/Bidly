2c1d5b661668a8d9bd031c4940cdde3b00650d3c
dIB59 <ibrahim@ebs.net.pk>
2023-07-23 10:59:29 +0200

    refactored createJobPostForUser

--- src/main/java/com/bidly/bidly/user/BidlyUserController.java
+++ src/main/java/com/bidly/bidly/user/BidlyUserController.java
@@ -10,10 +10,7 @@ import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import java.io.IOException;
-import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -46,10 +43,9 @@ public class BidlyUserController {
                                                   @RequestParam("description") String description,
                                                   @RequestParam("location") String location,
                                                   @RequestParam("materials") String materialsStr) {
-        boolean materials = Boolean.parseBoolean(materialsStr);
-        JobRequestDto job = new JobRequestDto(title, description, location, "http-test", materials);
+
         try {
-            return service.addJobPostToUser(userSubject, oidcUser, job, file);
+            return service.addJobPostToUser(userSubject, file, title, description, location, materialsStr);
         } catch (IOException e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
