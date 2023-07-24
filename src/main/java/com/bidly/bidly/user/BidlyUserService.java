5fa446047e6c3f6159177a2f27c7c972a1d2737d
dIB59 <ibrahim@ebs.net.pk>
2023-07-24 11:59:15 +0200

    extracted helper method

--- src/main/java/com/bidly/bidly/user/BidlyUserService.java
+++ src/main/java/com/bidly/bidly/user/BidlyUserService.java
@@ -1,30 +1,17 @@
 package com.bidly.bidly.user;
 
 import com.bidly.bidly.job.Job;
-import com.bidly.bidly.job.JobRequestDto;
 import com.bidly.bidly.job.JobRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.util.Pair;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.parameters.P;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
-import java.nio.file.Paths;
-import java.util.Objects;
+import static com.bidly.bidly.util.HelperMethods.getFileUrl;
 
 @Service
 @Transactional
@@ -64,20 +51,5 @@ public class BidlyUserService {
                 .toUri();
         return Pair.of(locationUri, job);
 
-    }
-
-    private String getFileUrl(MultipartFile file) throws IOException {
-        String fileUrl = null;
-        if (file != null) {
-            String fileName = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
-            String directory = "src/main/resources/static/job-images/";
-            String filePath = Paths.get(directory, fileName).toString();
-            fileUrl = "job-images/" + fileName;
-
-            BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(filePath)));
-            stream.write(file.getBytes());
-            stream.close();
-        }
-        return fileUrl;
     }
 }
