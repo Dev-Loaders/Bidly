a204e2d524e1d70c608f677c4381b69d624a06d2
dIB59 <ibrahim@ebs.net.pk>
2023-07-24 11:54:00 +0200

    implemented PUT requests

--- src/main/java/com/bidly/bidly/job/JobController.java
+++ src/main/java/com/bidly/bidly/job/JobController.java
@@ -1,9 +1,14 @@
 package com.bidly.bidly.job;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
 import java.util.List;
+import static com.bidly.bidly.util.HelperMethods.getFileUrl;
 
 @RestController
 @RequestMapping("/api/jobs")
@@ -35,4 +40,28 @@ public class JobController {
 
         return ResponseEntity.ok(job);
     }
+
+    @PutMapping("/{jobId}")
+    public ResponseEntity<Job> updateJob(@PathVariable String jobId,
+                                         @RequestParam(value = "image", required = false) MultipartFile file,
+                                         @RequestParam("title") String title,
+                                         @RequestParam("description") String description,
+                                         @RequestParam("location") String location,
+                                         @RequestParam("materials") String materialsStr) {
+
+
+        try {
+            String fileUrl = getFileUrl(file);
+            Job updatedJob = new Job(title, description, location, fileUrl, Boolean.parseBoolean(materialsStr));
+            Job job = service.updateJob(jobId, updatedJob);
+            if (job == null) {
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok(job);
+        } catch (IOException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+    }
+
+
 }
