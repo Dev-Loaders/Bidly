fa97cfc0a96bf4216b6914bfedc3d38755a82cca
dIB59 <ibrahim@ebs.net.pk>
2023-07-26 15:47:14 +0200

    now upload files to Azure blob

--- src/main/java/com/bidly/bidly/azure/AzureBlobService.java
+++ src/main/java/com/bidly/bidly/azure/AzureBlobService.java
@@ -1,2 +1,39 @@
-package com.bidly.bidly.azure;public class AzureBlobService {
+package com.bidly.bidly.azure;
+
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Service
+public class AzureBlobService {
+
+    @Value("${azure.storage.accountName}")
+    private String accountName;
+
+    @Value("${azure.storage.accountKey}")
+    private String accountKey;
+
+    public String uploadMultipartFileToBlob(MultipartFile file, String fileName) throws IOException {
+
+        String connectionString = String.format("DefaultEndpointsProtocol=https;AccountName=%s;AccountKey=%s;EndpointSuffix=core.windows.net",
+                accountName, accountKey);
+        System.out.println(connectionString);
+        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
+                .connectionString(connectionString)
+                .buildClient();
+        BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient("images");
+        BlobClient blobClient = containerClient.getBlobClient(fileName);
+        try (InputStream is = file.getInputStream()) {
+            blobClient.upload(is, file.getSize(), true);
+        }
+        return blobClient.getBlobUrl();
+    }
 }
