252fb66b210f558671832304050dae696d9187b1
Vanessa Wingårdh <vwingardh@gmail.com>
2023-07-26 13:04:34 +0200

    Refactor: Styling of images and formatting

--- client/app/workspace/layout.tsx
+++ client/app/workspace/layout.tsx
@@ -8,7 +8,6 @@ export default function WorkspaceLayout({
 }: {
   children: React.ReactNode;
 }) {
-
   return (
     <>
       <Navbar expand="lg" style={{ backgroundColor: "#242424" }}>
@@ -16,33 +15,45 @@ export default function WorkspaceLayout({
           <Link
             href={`/workspace`}
             style={{
-              color: '#fff',
+              color: "#fff",
               textDecoration: "none",
-              fontFamily: 'Vollkorn',
-              fontWeight: '600',
+              fontFamily: "Vollkorn",
+              fontWeight: "600",
               marginLeft: "30px",
             }}
           >
             Bidly
           </Link>
         </Navbar.Brand>
-        <Navbar.Toggle className="custom-toggler" aria-controls="basic-navbar-nav" style={{ marginRight: '30px', color: "#fff" }} />
-        <Navbar.Collapse id="basic-navbar-nav" style={{ color: '#fff' }}>
-          <Nav className="mr-auto" style={{ marginLeft: '30px' }}>
-            <Nav.Link style={{ color: '#fff' }} href={`/workspace`}>All Projects</Nav.Link>
-            <Nav.Link style={{ color: '#fff' }} href={`/workspace/post-job`}>Publish Project</Nav.Link>
-            <Nav.Link style={{ color: '#fff' }} href={`/workspace/user-jobs`}>
+        <Navbar.Toggle
+          className="custom-toggler"
+          aria-controls="basic-navbar-nav"
+          style={{ marginRight: "30px", color: "#fff" }}
+        />
+        <Navbar.Collapse id="basic-navbar-nav" style={{ color: "#fff" }}>
+          <Nav className="mr-auto" style={{ marginLeft: "30px" }}>
+            <Nav.Link style={{ color: "#fff" }} href={`/workspace`}>
+              All Projects
+            </Nav.Link>
+            <Nav.Link style={{ color: "#fff" }} href={`/workspace/publish-project`}>
+              Publish Project
+            </Nav.Link>
+            <Nav.Link style={{ color: "#fff" }} href={`/workspace/user-projects`}>
               Your Projects
             </Nav.Link>
-            <Nav.Link style={{ color: '#fff' }} href={`/workspace/user-bids`}>
+            <Nav.Link style={{ color: "#fff" }} href={`/workspace/bids`}>
               Your Bids
             </Nav.Link>
-            </Nav>
-            <Nav className="ml-auto">
-            <Nav.Link className="me-4" style={{ color: '#fff' }} href={`http://localhost:8080/login?logout`}>
+          </Nav>
+          <Nav className="ml-auto">
+            <Nav.Link
+              className="me-4"
+              style={{ color: "#fff" }}
+              href={`http://localhost:8080/login?logout`}
+            >
               Logout
             </Nav.Link>
-            </Nav>
+          </Nav>
         </Navbar.Collapse>
       </Navbar>
       <main>{children}</main>
